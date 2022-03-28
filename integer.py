30f6689640f191c0c2c05c51840dc7524171c4c6
Ubuntu <ubuntu@ip-172-31-80-10.ec2.internal>
2022-03-28 21:02:52 +0000

    Rodas

---
+++ integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by Peter Gemmell
+# Created on March 2022
+# This program calculates the circumference of a circle using constants
+
+
+def main():
+    # this function can tell the user what the charge of the integer is
+
+    # input
+    number = int(input("Enter any integer: "))
+    print("")
+
+    # process & output
+    if number > 0:
+        print("The number {} is positive (+)".format(number))
+    elif number < 0:
+        print("The Number {} is Negative (-)".format(number))
+    elif number == 0:
+        print("Number is 0")
+    else:
+        print("error code: \nANTEATER")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
